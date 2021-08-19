61b9d15ca3bd80cc49a13d46a3569288f7adf00d
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-08-19 23:12:07 +0300

    Finds location giving longitude and latitude

--- app/src/main/java/com/lucky/location/MainActivity.kt
+++ app/src/main/java/com/lucky/location/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.lucky.location
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.lucky.location.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object {
+        const val REQUEST_LOCATION = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        /*We just created a fused location services client on the statement above*/
+
+        binding.btnLocation.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+//                return@setOnClickListener
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    REQUEST_LOCATION
+                )
+            } else {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        if (location != null) {
+                            binding.txtLocation.text =
+                                "Latitude: ${location.latitude}\n Longitude: ${location.longitude}"
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "Cannot trace location, still in production mode",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+            }
+        }
+
     }
+
 }
